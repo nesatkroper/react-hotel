ae593e1f25833716da968043c1cc143acd533c9c
Nesatkroper <145594382+nesatkroper@users.noreply.github.com>
2025-02-14 13:19:12 +0700

    Update api-url.js

--- src/providers/api-url.js
+++ src/providers/api-url.js
@@ -1,2 +1,2 @@
-// export const apiUrl = "https://phanun.up.railway.app/api";
-export const apiUrl = "http://localhost:3000/api";
+export const apiUrl = "https://phanun.up.railway.app/api";
+// export const apiUrl = "http://localhost:3000/api";
