a65b67d855c9f9435a7cb15d583672b181f1acfe
megalobob <phanunsuon@gmail.com>
2025-04-08 23:04:16 +0700

    modified:   .env 	modified:   src/pages/admin/pos/components/pos-search.jsx

--- src/pages/admin/pos/components/pos-search.jsx
+++ src/pages/admin/pos/components/pos-search.jsx
@@ -1,123 +1,44 @@
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-} from "@/components/ui/command";
-import { AlertDialog, AlertDialogTrigger } from "@/components/ui/alert-dialog";
-import { Check, ChevronsUpDown, Search } from "lucide-react";
-import { cn } from "@/lib/utils";
-import React, { useEffect, useState } from "react";
-import { Label } from "@/components/ui/label";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { useSelector, useDispatch } from "react-redux";
-import { getSearchCate } from "@/contexts/reducer/search-category-slice";
-import { getCategorys } from "@/contexts/reducer/product-category-slice";
+import React, { useEffect } from "react";
 import OpenShift from "../../shift/open";
 import CloseShift from "../../shift/close";
 import PropTypes from "prop-types";
+import { AlertDialog, AlertDialogTrigger } from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import { useSelector, useDispatch } from "react-redux";
+// import { getSearchCate } from "@/contexts/reducer/search-category-slice";
+import { getCategorys } from "@/contexts/reducer/product-category-slice";
+import { FormComboBox, FormInput } from "@/components/app/form";
 
 const POSSearch = ({ shift }) => {
   const dispatch = useDispatch();
-  const { pcaData } = useSelector((state) => state?.pcategories);
-  const [open, setOpen] = useState(false);
-  const [value, setValue] = useState("");
-  const [searchTerm, setSearchTerm] = useState("");
+  const { data: pcaData } = useSelector((state) => state?.pcategories);
+  // const [open, setOpen] = useState(false);
+  // const [value, setValue] = useState("");
+  // const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     dispatch(getCategorys());
   }, [dispatch]);
 
   return (
-    <div className="flex justify-between">
-      <div className="flex gap-6">
-        <div className="flex flex-col gap-2">
-          {" "}
-          <Label>Product Category</Label>
-          <Popover open={open} onOpenChange={setOpen}>
-            <PopoverTrigger asChild>
-              <Button
-                variant="outline"
-                role="combobox"
-                aria-expanded={open}
-                className="w-[250px] justify-between"
-              >
-                {value
-                  ? pcaData?.find(
-                      (room) => String(room.product_category_id) === value
-                    )?.category_name
-                  : "All Category"}
-                <ChevronsUpDown className="opacity-50" />
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-[250px] p-0">
-              <Command>
-                <CommandInput
-                  placeholder="Search product category..."
-                  className="h-9"
-                />
-                <CommandList>
-                  <CommandEmpty>No Product Category found.</CommandEmpty>
-
-                  <CommandGroup>
-                    {pcaData?.map((cate) => (
-                      <CommandItem
-                        key={cate.product_category_id}
-                        onClick={() =>
-                          console.log(String(cate.product_category_id))
-                        }
-                        value={String(cate.product_category_id)}
-                        onSelect={(currentValue) => {
-                          setValue(currentValue);
-                          setOpen(false);
-                        }}
-                      >
-                        {cate.category_name}
-                        <Check
-                          className={cn(
-                            "ml-auto",
-                            value === cate.product_category_id
-                              ? "opacity-100"
-                              : "opacity-0"
-                          )}
-                        />
-                      </CommandItem>
-                    ))}
-                  </CommandGroup>
-                </CommandList>
-              </Command>
-            </PopoverContent>
-          </Popover>
+    <div className='flex justify-between'>
+      <div className='flex gap-6'>
+        <div className='flex flex-col gap-2'>
+          <FormComboBox
+            item={pcaData}
+            optID='categoryId'
+            optLabel='categoryName'
+            label='Product Category'
+          />
         </div>
-        <div className="flex flex-col gap-2">
-          <Label>Search Product</Label>
-          <div className="flex gap-1">
-            <Input
-              type="text"
-              name="search"
-              className="w-[350px]"
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-              placeholder="Search your wish product here"
-            />
-            <Button onClick={() => dispatch(getSearchCate(4))}>
-              <Search />
-            </Button>
-          </div>
+        <div className='flex flex-col gap-2'>
+          <FormInput label='Search ...' />
         </div>
       </div>
-      <div className="flex gap-2 items-end">
+      <div className='flex gap-2 items-end'>
         <AlertDialog>
           <AlertDialogTrigger disabled={shift ? true : false}>
-            <Button disabled={shift ? true : false} className="bg-green-500">
+            <Button disabled={shift ? true : false} className='bg-green-500'>
               Open Shift
             </Button>
           </AlertDialogTrigger>
@@ -125,7 +46,7 @@ const POSSearch = ({ shift }) => {
         </AlertDialog>
         <AlertDialog>
           <AlertDialogTrigger disabled={shift ? false : true}>
-            <Button disabled={shift ? false : true} className="bg-red-500">
+            <Button disabled={shift ? false : true} className='bg-red-500'>
               Close Shift
             </Button>
           </AlertDialogTrigger>
